90613bf49d44347d520a9524a1aae95dfcfc0230
Feng-Yz <1150125812@qq.com>
2021-07-14 23:53:39 +0800

    [A, M]

--- Codes/non_linear_equation.py
+++ Codes/non_linear_equation.py
@@ -1,4 +1,14 @@
+import math
+
+
 def fixpt(g, p0, tol, max1):
+    """
+    :param g: the iteration function
+    :param p0: the initial point
+    :param tol: the tolerance of the error
+    :param max1: the maximum number of the iterations
+    :return: (the number of iterations, the approximation to the fixed point, the sequence pn, the absolute error, the relative error)
+    """
     P = [p0]
     n = 0
     for k in range(max1):
@@ -6,9 +16,98 @@ def fixpt(g, p0, tol, max1):
         P.append(pk)
         n += 1
         err = abs(P[-1] - P[-2])
-        relerr = abs(P[-1] - P[-2]) / abs(P[-1])
-        if  err < tol or relerr < tol:
+        try:
+            relerr = abs(P[-1] - P[-2]) / abs(P[-1])
+        except ZeroDivisionError:
+            pass
+        if err < tol or relerr < tol:
             break
-    if n == max1:
+    if k + 1 == max1:
         print("Maximum number of iterations exceeded!")
-    return P
+    return k + 1, P[-1], P, err, relerr
+
+
+def bisect(f, a, b, delta):
+    """
+    :param f: the function
+    :param a: the left point
+    :param b: the right point
+    :param delta: the tolerance
+    :return: (the number of iterations, the approximation to the solution x0, the absolute error, the value f(x0))
+    """
+    if f(a)*f(b) > 0:
+        print("Note: f(a)*f(b) > 0")
+        return
+    max1 = round((math.log(b - a) - math.log(delta)) / math.log(2))
+    for k in range(max1):
+        c = (a + b) / 2
+        if f(c) == 0:
+            a = c
+            b = c
+        elif f(a)*f(c) > 0:
+            a = c
+        else:
+            b = c
+        if b - a < delta:
+            break
+    c = (a + b) / 2
+    err = abs(b - a)
+    return k + 1, c, err, f(c)
+
+
+def regula(f, a, b, epsilon, max1):
+    """
+    :param f: the function
+    :param a: the left point
+    :param b: the right point
+    :param epsilon: the tolerance
+    :param max1: the maximum number of the iterations
+    :return: (the number of iterations, the approximation to the solution x0, the value f(x0))
+    """
+    if f(a)*f(b) > 0:
+        print("Note: f(a)*f(b) > 0")
+        return
+    for k in range(max1):
+        c = b - (f(b) * (b - a)) / (f(b) - f(a))
+        if f(c) == 0:
+            a = c
+            b = c
+        elif f(a)*f(c) > 0:
+            a = c
+        else:
+            b = c
+        if abs(f(c)) < epsilon:
+            break
+    c = (a + b) / 2
+    err = abs(b - a)
+    return k + 1, c, f(c)
+
+
+def approot(f, X, epsilon):
+    Y = list(map(f, X))
+    yrange = max(Y) - min(Y)
+    epsilon2 = yrange * epsilon
+    n = len(X)
+    R = []
+    X.append(X[-1]); Y.append(Y[-1])
+    for i in range(1, len(X)-1):
+        if Y[i]*Y[i-1] <= 0:
+            R.append((X[i] + X[i-1]) / 2)
+        s = (Y[i] - Y[i-1]) * (Y[i+1] - Y[i])
+        if abs(Y[i]) < epsilon2 and s <= 0:
+            R.append(X[i])
+    return R
+
+
+def newton(f, df, p0, delta, epsilon, max1):
+    for k in range(max1):
+        p1 = p0 - f(p0) / df(p0)
+        err = abs(p0 - p1)
+        try:
+            relerr = err / abs(p1)
+        except ZeroDivisionError:
+            pass
+        p0 = p1
+        if err < delta or relerr < delta or abs(f(p0)) < epsilon:
+            break
+    return k + 1, p0, err, f(p0)
--- Notes/[2]求解x=g(x)的迭代法.md
+++ Notes/[2]求解x=g(x)的迭代法.md
@@ -2,15 +2,15 @@
 
 Definition 1 函数$g(x)$**不动点**是指一个实数$P$ s.t. $P = g(P)$.
 
-Definition 2 迭代$p_{n+1} = g(p_{n}), n = 0,1,\cdots$称为不动点迭代。
+Definition 2 迭代$p_{n+1} = g(p_{n}), n = 0,1,\cdots$称为不动点迭代. 
 
-Theorem 1 $g$是一个连续函数，$p_n$是由不动点迭代生成的数列。如果$\lim_{n\rightarrow +\infin} {p_n}= P$，则$P$是$g(x)$的不动点。
+Theorem 1 $g$是一个连续函数，$p_n$是由不动点迭代生成的数列. 如果$\lim_{n\rightarrow +\infin} {p_n}= P$，则$P$是$g(x)$的不动点. 
 
 Proof:
 $$
 g(P) = g(\lim_{n\rightarrow +\infin} {p_n}) = \lim_{n\rightarrow +\infin}g(p_n) = \lim_{n\rightarrow +\infin}p_{n+1} = \lim_{n\rightarrow +\infin}p_{n} = P
 $$
-Theorem 2 函数$g\in C[a,b]$. 若$x\in [a, b]$时，$g(x)\in [a, b]$，则$g$在$[a, b]$内存在不动点；且若$g\in D[a,b]$且存在$0<K<1$，使$|g\prime(x)| \leq K < 1$，则$g$在$[a, b]$内有唯一的不动点。
+Theorem 2 函数$g\in C[a,b]$. 若$x\in [a, b]$时，$g(x)\in [a, b]$，则$g$在$[a, b]$内存在不动点；且若$g\in D[a,b]$且存在$0<K<1$，使$|g\prime(x)| \leq K < 1$，则$g$在$[a, b]$内有唯一的不动点. 
 
 Proof:
 
@@ -28,7 +28,7 @@ Theorem 3
 
 设有（i）$g,g\prime \in C[a, b]$，（ii）$K>0$，（iii）$p_0 \in (a, b)$，（iv）$g(x)\in [a, b]$ s.t. $x\in [a, b]$.
 
-若$|g\prime(x)| \leq K < 1$，则迭代$p_{n+1} = g(p_{n})$将收敛到唯一的不动点$P$，$P$被称为吸引不动点；若$|g\prime(x)| > 1$，则迭代$p_{n+1} = g(p_{n})$将不会收敛到$P$，$P$被称为排斥不动点。
+若$|g\prime(x)| \leq K < 1$，则迭代$p_{n+1} = g(p_{n})$将收敛到唯一的不动点$P$，$P$被称为吸引不动点；若$|g\prime(x)| > 1$，则迭代$p_{n+1} = g(p_{n})$将不会收敛到$P$，$P$被称为排斥不动点. 
 
 Proof:
 $$
